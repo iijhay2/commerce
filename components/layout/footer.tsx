8ad790099486f3ba9d816ebe67edbf130e951b35
iijhay2 <iijhay26@gmail.com>
2023-12-29 02:33:24 +0800

    Commit for footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link';
+
 import FooterMenu from 'components/layout/footer-menu';
+import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
 import { Suspense } from 'react';
 
@@ -14,6 +17,12 @@ export default async function Footer() {
   return (
     <footer className="text-sm text-neutral-500 dark:text-neutral-400">
       <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
+        <div>
+          <Link className="flex items-center gap-2 text-black dark:text-white md:pt-1" href="/">
+            <LogoSquare size="sm" />
+            <span className="uppercase">{SITE_NAME}</span>
+          </Link>
+        </div>
         <Suspense
           fallback={
             <div className="flex h-[188px] w-[200px] flex-col gap-2">
