92d0a8385a0e5b6bf607f2a163c468fbe5c569dc
iijhay2 <iijhay26@gmail.com>
2023-12-29 16:46:45 +0800

    For logo fix commit

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -1,21 +1,18 @@
 import clsx from 'clsx';
 import LogoIcon from './icons/logo';
 
-export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
+export default function LogoSquare({ size }: { size?: 'lg' | undefined }) {
   return (
     <div
-      className={clsx(
-        'flex flex-none items-center justify-center border border-neutral-200 bg-white dark:border-neutral-700 dark:bg-black',
-        {
-          'h-[40px] w-[40px] rounded-xl': !size,
-          'h-[30px] w-[30px] rounded-lg': size === 'sm'
-        }
-      )}
+      className={clsx('flex flex-none items-center justify-center', {
+        'h-[40px] w-[40px]': !size,
+        'h-[auto]] w-[100%]': size === 'lg'
+      })}
     >
       <LogoIcon
         className={clsx({
           'h-[16px] w-[16px]': !size,
-          'h-[10px] w-[10px]': size === 'sm'
+          'h-[auto] w-[100%]': size === 'lg'
         })}
       />
     </div>
